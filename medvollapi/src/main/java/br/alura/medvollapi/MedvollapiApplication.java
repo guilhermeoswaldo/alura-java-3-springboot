180a82cc5ed95e98477b308ac6d85feda15a0111
guilhermeoswaldo <guilhermeoswaldo@touchtec.com.br>
2026-03-10 11:30:56 -0300

    Início do curso Spring Boot 3 - springboot: Aula 1 - Inicializando projeto

---
+++ medvollapi/src/main/java/br/alura/medvollapi/MedvollapiApplication.java
@@ -0,0 +1,13 @@
+package br.alura.medvollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedvollapiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MedvollapiApplication.class, args);
+	}
+
+}
---
+++ medvollapi/src/test/java/br/alura/medvollapi/MedvollapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.alura.medvollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedvollapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
